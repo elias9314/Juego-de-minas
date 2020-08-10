06047065437a26c1388a450e8b5516ea68b10331
AlexMejiaYavirac <apa.mejia@yavirac.edu.ec>
2020-08-09 22:22:46 -0500

    Funcion para mostrar daots en la matris

--- Controllers/game.js
+++ Controllers/game.js
@@ -1,6 +1,6 @@
 var minas = inicializaMatriz();
 
-//Funcion
+//Funcion matriz
 function inicializaMatriz() {
     var tabla = [];
     for (var i = 0; i < 10; i++) {
@@ -20,4 +20,26 @@ function crearTablero() {
         }
     }
 
+}
+
+//Mostar numeros 
+
+function mostrarNumero(e) {
+    var auxstr = this.id.split("");
+    var myid = auxstr[0] + auxstr[1];
+    divObj = document.getElementById(myid);
+
+    if (minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] == 0) {
+        divObj.style.backgroundColor = "white";
+        abrirAlrededor(parseInt(auxstr[0], 10), parseInt(auxstr[1], 10), minas);
+    } else {
+        if (minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] != "*") {
+            document.getElementById(myid).innerHTML = "<p style='margin-top:15px;'>" + minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] + "</p>";
+            divObj.style.backgroundColor = "white";
+        } else {
+            divObj.style.backgroundImage = "url(../images/bomba.jpg)";
+            abrirTablero(minas);
+            setTimeout(function(){ location.reload(); }, 300);
+        }
+    }
 }
