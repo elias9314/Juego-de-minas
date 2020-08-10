af8cdd2abdeef4bb1cb562f6b8b9b77e1a2af5cf
elias9314 <kuky1993roger@gmail.com>
2020-08-09 21:33:35 -0500

    funcion para generar el tablero
    esta funcion nos permitira crear el tablero en el div recorriendo el numero de la matriz tanto horizontal y vertical

--- Controllers/game.js
+++ Controllers/game.js
@@ -8,3 +8,16 @@ function inicializaMatriz() {
     }
     return tabla;
 }
+
+//// Funcion para crear el tablero para el juego de minas////
+function crearTablero() {
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            var div = document.createElement("div");
+            div.id = i + "" + j;
+            div.addEventListener("click", mostrarNumero, true);
+            tablerominas.appendChild(div);
+        }
+    }
+
+}
