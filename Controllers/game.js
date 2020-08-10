0766ecd0d65266155fc1dabeec3499cb7c0221d6
elias9314 <kuky1993roger@gmail.com>
2020-08-09 23:10:15 -0500

    Funcion numero de bombas
    Coloca el numero de bombas existentes que hay alrededor de las casillas

--- Controllers/game.js
+++ Controllers/game.js
@@ -37,9 +37,51 @@ function mostrarNumero(e) {
             document.getElementById(myid).innerHTML = "<p style='margin-top:15px;'>" + minas[parseInt(auxstr[0], 10)][parseInt(auxstr[1], 10)] + "</p>";
             divObj.style.backgroundColor = "white";
         } else {
-            divObj.style.backgroundImage = "url(../images/bomba.jpg)";
+            divObj.style.backgroundImage = "url(../images/bomba.png)";
             abrirTablero(minas);
             setTimeout(function(){ location.reload(); }, 300);
+        }
+    }
+}
+
+//funcion para conocer el numero de bombas que hay alrededor 
+function bombasAlrededor(tablero) {
+    for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
+            if (tablero[i][j] == "*") {
+                if (i == 0 && j == 0) {
+                    colocaNumeroBombas(i, j, i + 1, j + 1, tablero);
+                }
+                else if (i == 0 && (j > 0 && j < 9)) {
+                    colocaNumeroBombas(i, j - 1, i + 1, j + 1, tablero);
+                }
+                else if (i == 0 && (j > 0 && j < 9)) {
+                    colocaNumeroBombas(i, j - 1, i + 1, j + 1, tablero);
+                }
+                else if (i == 0 && (j > 0 && j < 9)) {
+                    colocaNumeroBombas(i, j - 1, i + 1, j + 1, tablero);
+                }
+                else if (i == 0 && j == 9) {
+                    colocaNumeroBombas(i, j - 1, i + 1, j, tablero);
+                }
+                else if (j == 9 && (i > 0 && i < 9)) {
+                    colocaNumeroBombas(i - 1, j - 1, i + 1, j, tablero);
+                }
+                else if (i == 9 && j == 9) {
+                    colocaNumeroBombas(i - 1, j - 1, i, j, tablero);
+                }
+                else if (i == 9 && (j > 0 && j < 9)) {
+                    colocaNumeroBombas(i - 1, j - 1, i, j + 1, tablero);
+                }
+                else if (i == 9 && j == 0) {
+                    colocaNumeroBombas(i - 1, j, i, j + 1, tablero);
+                }
+                else if (j == 0 && (i > 0 && i < 9)) {
+                    colocaNumeroBombas(i - 1, j, i + 1, j + 1, tablero);
+                } else {
+                    colocaNumeroBombas(i - 1, j - 1, i + 1, j + 1, tablero);
+                }
+            }
         }
     }
 }
