ac2c886bfdbc50db070dbea0cbb377ffd321d514
elias9314 <kuky1993roger@gmail.com>
2020-08-11 18:31:34 -0500

    cambio login

--- Controllers/login.js
+++ Controllers/login.js
@@ -1,7 +1,5 @@
 function  login(){ 
-    var user = document.getElementById("username").value   
-    localStorage.setItem("SecretUser",user)
-    window.location.replace("http://localhost/JUEGO-DE-MINAS/Views/game.html")   
-
-
+     var user = document.getElementById("username").value   
+     localStorage.setItem("SecretUser",user)
+    window.location.replace("http://localhost/JUEGO-DE-MINAS/Views/game.html")  
 }
